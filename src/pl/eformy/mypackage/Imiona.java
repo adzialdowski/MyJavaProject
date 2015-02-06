1aca1d70adecd662f046eaf083123896ac062648
Andrzej <andrzej.gd@gmail.com>
2015-02-06 16:02:34 +0100

    ABC

---
+++ src/pl/eformy/mypackage/Imiona.java
@@ -0,0 +1,15 @@
+package pl.eformy.mypackage;
+
+/**
+ * Created by Andrzej on 2015-02-06.
+ */
+public class Imiona {
+
+    public String getAndrzej() {
+        return "Andrzej";
+    }
+
+    public String getWiolka() {
+        return "Wiolka";
+    }
+}
